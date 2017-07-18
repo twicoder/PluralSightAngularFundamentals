8eb05570e13bc49ad45b9b0c4f788fc178dfcbdc
twicoder <renqingwei123@163.com>
2017-07-18 17:21:25 +0800

    Angular basic project

---
+++ app/js/controllers/EventController.js
@@ -0,0 +1,30 @@
+'use strict';
+
+eventsApp.controller('EventController',
+    function EventController($scope, eventData,$log){
+
+        $scope.snippet = '<span style="color:red">Hi,there!</span>';
+        $scope.boolValue = true;
+        $scope.mystyle = {color:'red'};
+        $scope.myclass = "blue";
+        $scope.buttonDisabled = true;
+
+        $scope.sortorder = 'name';
+
+        $scope.event = eventData.getEvent();
+
+        eventData.getEvent()
+            .$promise
+            .then(function(event){$scope.event = event; console.log(event);})
+            .catch(function(response){console.log(response)});
+
+        $scope.upVoteSession = function(session){
+            session.upVoteCount++;
+        };
+
+        $scope.downVoteSession = function(session){
+            session.upVoteCount--;
+        };
+
+    }
+);
